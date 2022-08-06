fec98f79f00abc59fe5c861b218d746e55b085f4
hamirdangar <109734153+hamirdangar@users.noreply.github.com>
2022-08-06 08:13:15 +0530

    Add files via upload

---
+++ assignment/module 1/DETA.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{	clrscr();
+	printf("my name is hamir dangar\n");
+	printf("my birth date is 02/12/2001\n");
+	printf("my age is 21 years\n");
+	printf("address is rajkot\n");
+	getch();
+}
