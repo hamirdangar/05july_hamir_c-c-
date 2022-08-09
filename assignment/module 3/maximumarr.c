58c3219099a31fd146db34f982a017d022cf93aa
hamirdangar <109734153+hamirdangar@users.noreply.github.com>
2022-08-09 10:16:45 +0530

    Add files via upload

---
+++ assignment/module 3/maximumarr.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10],i,max;
+    printf("enter the 10 elements");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        if(arr[i]>arr[0])
+        {
+            arr[0]=arr[i];
+
+        }
+        max=arr[0];
+    }
+    printf("maximum number is %d",arr[0]);
+    return 0;
+}
