09141ee20320c7687d33a2a932c0f83adc964687
hamirdangar <109734153+hamirdangar@users.noreply.github.com>
2022-09-07 16:31:20 +0530

    Add files via upload

---
+++ task/pattern7.java
@@ -0,0 +1,16 @@
+public class pattern7 {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=i;k>=1;k--){
+                System.out.print(k);
+            }
+            for(int l=2;l<=i;l++){
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+    }
+}
